9753f27c038a0142aface97dd4daa6de720e9b98
astarikov <artemoficial@gmail.com>
2015-12-18 11:41:33 +0300

    initial commit

---
+++ src/main/java/ru/solrmndr/SolRmndr.java
@@ -0,0 +1,11 @@
+package ru.solrmndr;
+
+import ru.solrmndr.frames.MainWindow;
+
+public class SolRmndr {
+
+    public static void main(String[] args) {
+        //Запуск приложения
+        new MainWindow();
+    }
+}
---
+++ src/main/java/ru/solrmndr/frames/MainWindow.java
@@ -0,0 +1,32 @@
+package ru.solrmndr.frames;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Основное окно приложения
+ */
+public class MainWindow extends JFrame {
+
+    public static final String MAIN_WINDOW_NAME = "Solution Remainder";
+    public static final String VERSION = "0.00001";
+    public static final int MAIN_WINDOW_WIDTH = 600;
+    public static final int MAIN_WINDOW_HEIGHT = 400;
+
+    public MainWindow() throws HeadlessException {
+        setTitle(MAIN_WINDOW_NAME + " v." + VERSION);
+        setSize(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+        initGui();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    /**
+     * В методе инициализируются компоненты которые содержат основные компоненты
+     */
+    protected void initGui() {
+
+    }
+
+
+}
