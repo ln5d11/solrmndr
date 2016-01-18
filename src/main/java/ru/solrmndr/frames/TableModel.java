9003f7a31f6d030f0c5eb471b169ddb1e34b9897
lns <mestfund@gmail.com>
2016-01-18 18:33:30 +0200

    Добавлены выпадающие списки для 3 и 6 колонки. Добавлен тестовый метод для верификации ввода даты для 4 колонки.

--- src/main/java/ru/solrmndr/frames/TableModel.java
+++ src/main/java/ru/solrmndr/frames/TableModel.java
@@ -1,39 +1,69 @@
 package ru.solrmndr.frames;
 
-import javax.swing.table.AbstractTableModel;
+import javax.swing.*;
+import javax.swing.table.*;
 
-public class TableModel extends AbstractTableModel {
+public class TableModel extends JTable {
+
+    DefaultTableModel tableModel = new DefaultTableModel();
 
     String[] columnNames = {"Название раствора", "Концентрация", "Ед. измерения", "Дата изготовления", "Годен до",
             "Особые условия", " "};
 
-    Object[][] data = {
-            {"C2H5OH", "10%", "ml", "21.12.15", "30.01.16", "-", " "}
-    };
+    Object[] data = {"C2H5OH", "10%", "мг/л", "11/12/12", "30.01.16", "-", " "};
 
-    @Override
-    public int getRowCount() {
-        return data.length;
+    private static final String DATE_REGEX = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)" +
+            "(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)" +
+            "?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])" +
+            "(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
+
+    public TableModel() {
+        setModel(tableModel);
+        initColumns();
+        //Добавляем тестовую длату, в будующем удалим
+        tableModel.addRow(data);
+        setUpDateCol(getColumnModel().getColumn(3));
+        setUpConditionCol(getColumnModel().getColumn(5));
+        setUpUnitCol(getColumnModel().getColumn(2));
     }
 
-    @Override
-    public int getColumnCount() {
-        return columnNames.length;
+
+    private void initColumns() {
+        for(String colName : columnNames) {
+            tableModel.addColumn(colName);
+        }
     }
 
-    @Override
-    public Object getValueAt(int row, int column) {
-        return data[row][column];
+    public void setUpDateCol (TableColumn dateCreationCol) {
+        for (int r=0; r < tableModel.getRowCount(); r++) {
+            for  (int c=0; c < tableModel.getColumnCount(); c++) {
+                String date = (String)getValueAt(r, 3);
+                if (date.matches(DATE_REGEX)) {
+                    dataModel.setValueAt(date, r, 3);
+                }
+                else {
+                    dataModel.setValueAt("Error", r, 3);
+                }
+            }
+        }
     }
 
-    @Override
-    public boolean isCellEditable(int row, int column) {
-        return true;
+    public void setUpUnitCol(TableColumn unitCol) {
+        JComboBox unitBox = new JComboBox();
+        unitBox.addItem("мг/л");
+        unitBox.addItem("мг/дм^3");
+        unitBox.addItem("моль/дм^3");
+        unitBox.addItem("ммоль/дм^3");
+        unitBox.addItem("мг/см^3");
+        unitCol.setCellEditor(new DefaultCellEditor(unitBox));
     }
 
-    @Override
-    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        data[rowIndex][columnIndex] = aValue;
-        super.setValueAt(aValue, rowIndex, columnIndex);
+    public void setUpConditionCol(TableColumn condCol){
+        JComboBox condBox = new JComboBox();
+        condBox.addItem("-");
+        condBox.addItem("Холодильник");
+        condBox.addItem("Вытяжка");
+        condCol.setCellEditor(new DefaultCellEditor(condBox));
     }
+
 }
